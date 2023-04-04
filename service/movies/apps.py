9595f52d1337ed6e5fad1d1947416e4cae2077da
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2023-04-04 23:34:54 +0300

    f

--- service/movies/apps.py
+++ service/movies/apps.py
@@ -5,3 +5,6 @@ class MoviesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "movies"
     verbose_name = "Cinema together"
+
+    def ready(self):
+        import movies.models.user  # noqa: F401
