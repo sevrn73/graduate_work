11cdfacc836d669dc18c5c2bafdcb28378fb302b
Zakhar Bogdanov <rachet2012@yandex.ru>
2023-04-12 00:33:57 +0300

    feat(friends)

--- auth_api/src/api/v1/api_v1_blueprint.py
+++ auth_api/src/api/v1/api_v1_blueprint.py
@@ -11,6 +11,7 @@ from src.api.v1.account import (
     refresh,
     sign_up,
 )
+from src.api.v1.friends import add_friend, delete_friend, friends_list
 from src.api.v1.managing import assign_role, detach_role, user_roles
 from src.api.v1.roles import change_role, create_role, delete_role, roles_list
 
@@ -34,3 +35,7 @@ app_v1_blueprint.add_url_rule("/roles_list", methods=["GET"], view_func=roles_li
 app_v1_blueprint.add_url_rule("/user_roles", methods=["GET"], view_func=user_roles)
 app_v1_blueprint.add_url_rule("/assign_role", methods=["POST"], view_func=assign_role)
 app_v1_blueprint.add_url_rule("/detach_role", methods=["DELETE"], view_func=detach_role)
+
+app_v1_blueprint.add_url_rule("/add_friend", methods=["POST"], view_func=add_friend)
+app_v1_blueprint.add_url_rule("/delete_friend", methods=["DELETE"], view_func=delete_friend)
+app_v1_blueprint.add_url_rule("/friends_list", methods=["GET"], view_func=friends_list)
